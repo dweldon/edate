1d45bbe81005e355a7b990c444356e690498379f
David Weldon <david.weldon@ask.com>
2010-07-02 12:57:33 -0700

    added shift/2 and shift/3.

--- README.markdown
+++ README.markdown
@@ -9,17 +9,27 @@ Installation
 
 Functions
 ---------
-### today
-returns the current date as {Year, Month, Day}.
-    > edate:today().
-    {2010,7,1}
+### today() -> Date
+returns today's date.
 
-### tomorrow
-returns tomorrow's date as {Year, Month, Day}.
-    > edate:tomorrow().
-    {2010,7,2}
+### tomorrow() -> Date
+returns tomorrow's date.
 
-### yesterday()
-returns yesterday's date as {Year, Month, Day}.
-    > edate:yesterday().
-    {2010,6,30}
+### yesterday() -> Date
+returns yesterday's date.
+
+### shift(N, Period) -> Date
+returns a new date after shifting today's date by N periods. N is an integer,
+Period is day | days | week | weeks | month | months | year | years
+
+### shift(StartDate, N, Period) -> Date
+returns a new date after shifting StartDate by N periods. N is an integer,
+Period is day | days | week | weeks | month | months | year | years
+    > edate:shift({2000,1,1}, -2, days)
+    {1999,12,30}
+    > edate:shift({2000,1,1}, 1, week)
+    {2000,1,8}
+    > edate:shift({2000,1,1}, 4, months)
+    {2000,5,1}
+    > edate:shift({2000,1,1}, 2, years)
+    {2002,1,1}
--- src/edate.erl
+++ src/edate.erl
@@ -13,23 +13,90 @@
 %% along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 -module(edate).
--export([today/0, tomorrow/0, yesterday/0]).
+-export([today/0,
+         tomorrow/0,
+         yesterday/0,
+         shift/2,
+         shift/3]).
 -include_lib("eunit/include/eunit.hrl").
 
-
-% @spec today() -> {Year, Month, Day}
+% @spec today() -> date()
 today() -> date().
 
-% @spec tomorrow() -> {Year, Month, Day}
-tomorrow() -> shift(date(), 1, day).
+% @spec tomorrow() -> date()
+tomorrow() -> shift(1, day).
 
-% @spec yesterday() -> {Year, Month, Day}
-yesterday() -> shift(date(), -1, day).
+% @spec yesterday() -> date()
+yesterday() -> shift(-1, day).
 
-% @spec shift(Date::date(), N::integer(), Type::type()) -> {Year, Month, Day}
-%       type() = day | days | week | weeks | month | months | year | years
-%       date() = {Year, Month, Day}
-shift(Date, N, days) ->
-    shift(Date, N, day);
-shift(Date, N, day) ->
-    calendar:gregorian_days_to_date(calendar:date_to_gregorian_days(Date) + N).
+% @spec shift(N::integer(), Period::period()) -> date()
+%       period() = day | days | week | weeks | month | months | year | years
+shift(N, Period) -> shift(date(), N, Period).
+
+% @spec shift(Date::date(), N::integer(), Period::period()) -> date()
+%       period() = day | days | week | weeks | month | months | year | years
+shift(Date, N, Period) when Period =:= day; Period =:= days ->
+    calendar:gregorian_days_to_date(calendar:date_to_gregorian_days(Date) + N);
+shift(Date, N, Period) when Period =:= week; Period =:= weeks ->
+    shift(Date, 7*N, days);
+shift(Date, N, Period) when Period =:= year; Period =:= years ->
+    shift(Date, 12*N, months);
+shift({Y, M, D}, N, Period) when Period =:= month; Period =:= months ->
+    % in order for the modular arithmetic to work, months in this function range
+    % from 0 to 11 (January to December)
+    TotalMonths = 12*Y + M-1 + N,
+    Month = TotalMonths rem 12,
+    Year = (TotalMonths - Month) div 12,
+    % add one back to the month to fix our mod 12 shenanigans
+    find_valid_date({Year, Month+1, D}).
+
+% @spec find_valid_date(Date::date()) -> date()
+% @doc returns Date if valid. otherwise, backward searches for a valid date.
+find_valid_date(Date) ->
+    case calendar:valid_date(Date) of
+        true -> Date;
+        false ->
+            {Y, M, D} = Date,
+            find_valid_date({Y, M, D-1})
+    end.
+
+shift_test_() ->
+    [?_assertEqual(date(), shift(0, days)),
+     ?_assertEqual(date(), shift(0, days)),
+     ?_assertEqual(date(), shift(0, months)),
+     ?_assertEqual(date(), shift(0, years)),
+     % relative dates
+     ?_assertEqual(tomorrow(), shift(1, day)),
+     ?_assertEqual(yesterday(), shift(-1, day)),
+     % simple day addition
+     ?_assertEqual({2000,1,2}, shift({2000,1,1}, 1, day)),
+     ?_assertEqual({2000,1,3}, shift({2000,1,1}, 2, days)),
+     % simple week addition
+     ?_assertEqual({2000,1,8}, shift({2000,1,1}, 1, week)),
+     ?_assertEqual({2000,1,15}, shift({2000,1,1}, 2, weeks)),
+     % simple month addition
+     ?_assertEqual({2000,2,1}, shift({2000,1,1}, 1, month)),
+     ?_assertEqual({2000,3,1}, shift({2000,1,1}, 2, months)),
+     % simple year addition
+     ?_assertEqual({2003,1,1}, shift({2000,1,1}, 3, years)),
+     % simple year subtraction
+     ?_assertEqual({1997,1,1}, shift({2000,1,1}, -3, years)),
+     % day subtraction at year boundary
+     ?_assertEqual({1999,12,31}, shift({2000,1,1}, -1, day)),
+     ?_assertEqual({1999,12,30}, shift({2000,1,1}, -2, days)),
+     % week subtraction at year boundary
+     ?_assertEqual({1999,12,25}, shift({2000,1,1}, -1, week)),
+     ?_assertEqual({1999,12,18}, shift({2000,1,1}, -2, weeks)),
+     % month subtraction at year boundary
+     ?_assertEqual({1999,12,1}, shift({2000,1,1}, -1, month)),
+     ?_assertEqual({1999,11,1}, shift({2000,1,1}, -2, months)),
+     % 1 year = 12 months = 365 days (in a non-leap year)
+     ?_assertEqual(shift({2001,5,10}, 1, year), shift({2001,5,10}, 12, months)),
+     ?_assertEqual(shift({2001,5,10}, 1, year), shift({2001,5,10}, 365, days)),
+     % date rounding from month addition and subtraction
+     ?_assertEqual({2001,2,28}, shift({2001,1,31}, 1, month)),
+     ?_assertEqual({2001,2,28}, shift({2001,3,31}, -1, month)),
+     % leap year
+     ?_assertEqual({2012,2,29}, shift({2012,1,31}, 1, month)),
+     ?_assertEqual({2012,2,29}, shift({2012,4,30}, -2, months)),
+     ?_assertEqual({2013,2,28}, shift({2012,2,29}, 1, year))].
