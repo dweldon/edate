f9fad4aa02f6255f8ac6554a2175eab271b90920
David Weldon <david.weldon@ask.com>
2010-10-21 17:45:33 -0700

    reordered functions. added proper edoc and typer annotation. changed comment format.

--- src/edate.erl
+++ src/edate.erl
@@ -16,98 +16,64 @@
 %% @doc edate is a date manipulation library.
 
 -module(edate).
--export([today/0,
-         tomorrow/0,
-         yesterday/0,
-         shift/2,
-         shift/3,
-         subtract/2,
+-export([beginning_of_month/1,
+         date_to_string/1,
+         day_of_week/1,
+         easter/1,
+         end_of_month/1,
          is_in_future/1,
          is_in_past/1,
-         beginning_of_month/1,
-         end_of_month/1,
-         date_to_string/1,
+         shift/2,
+         shift/3,
          string_to_date/1,
-         day_of_week/1]).
--export([easter/1]).
+         subtract/2,
+         today/0,
+         tomorrow/0,
+         yesterday/0]).
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
-% @spec today() -> date()
-today() -> date().
+-type year()    :: non_neg_integer().
+-type month()   :: 1..12.
+-type day()     :: 1..31.
+-type date()    :: {year(), month(), day()}.
+-type period()  :: day | days | week | weeks | month | months | year | years.
 
-% @spec tomorrow() -> date()
-tomorrow() -> shift(1, day).
+%% @type year() = integer().
+%% @type month() = integer(). 1..12
+%% @type day() = integer(). 1..31
+%% @type date() = {year(), month(), day()}.
+%% @type period() = day | days | week | weeks | month | months | year | years.
 
-% @spec yesterday() -> date()
-yesterday() -> shift(-1, day).
-
-% @spec shift(N::integer(), Period::period()) -> date()
-%       period() = day | days | week | weeks | month | months | year | years
-shift(N, Period) -> shift(date(), N, Period).
-
-% @spec shift(Date::date(), N::integer(), Period::period()) -> date()
-%       period() = day | days | week | weeks | month | months | year | years
-shift(Date, N, Period) when Period =:= day; Period =:= days ->
-    calendar:gregorian_days_to_date(calendar:date_to_gregorian_days(Date) + N);
-shift(Date, N, Period) when Period =:= week; Period =:= weeks ->
-    shift(Date, 7*N, days);
-shift(Date, N, Period) when Period =:= year; Period =:= years ->
-    shift(Date, 12*N, months);
-shift({Y, M, D}, N, Period) when Period =:= month; Period =:= months ->
-    % in order for the modular arithmetic to work, months in this function range
-    % from 0 to 11 (January to December)
-    TotalMonths = 12*Y + M-1 + N,
-    Month = TotalMonths rem 12,
-    Year = (TotalMonths - Month) div 12,
-    % add one back to the month to fix our mod 12 shenanigans
-    find_valid_date({Year, Month+1, D}).
-
-% @spec find_valid_date(Date::date()) -> date()
-% @doc returns Date if valid. otherwise, backward searches for a valid date.
-find_valid_date(Date) ->
-    case calendar:valid_date(Date) of
-        true -> Date;
-        false ->
-            {Y, M, D} = Date,
-            find_valid_date({Y, M, D-1})
-    end.
-
-% @spec subtract(Date1::date(), Date2::date()) -> integer()
-subtract(Date1, Date2) ->
-    calendar:date_to_gregorian_days(Date1) -
-    calendar:date_to_gregorian_days(Date2).
-
-% @spec is_in_future(Date) -> bool()
-is_in_future(Date) -> subtract(Date, today()) > 0.
-
-% @spec is_in_past(Date) -> bool()
-is_in_past(Date) -> subtract(Date, today()) < 0.
-
-% @spec beginning_of_month(Date::date()) -> date()
+%% @spec beginning_of_month(Date::date()) -> date()
+%% @doc Returns the first date of the month containing `Date'.
+%% ```
+%% > edate:beginning_of_month({2010,2,15}).
+%% {2010,2,1}
+%% '''
+-spec beginning_of_month(date()) -> date().
 beginning_of_month({Y, M, _D}) -> {Y, M, 1}.
 
-% @spec end_of_month(Date::date()) -> date()
-end_of_month({Y, M, _D}) -> {Y, M, calendar:last_day_of_the_month(Y, M)}.
-
-% @spec date_to_string(Date::date()) -> string()
+%% @spec date_to_string(Date::date()) -> string()
+%% @doc Returns an <a href="http://en.wikipedia.org/wiki/Iso8601">ISO 8601</a>
+%% representation of Date.
+%% ```
+%% > edate:date_to_string({1976,3,18}).
+%% "1976-03-18"
+%% '''
+-spec date_to_string(date()) -> string().
 date_to_string({Y, M, D}) ->
     true = calendar:valid_date({Y, M, D}),
     lists:flatten(io_lib:format("~4..0B-~2..0B-~2..0B", [Y, M, D])).
 
-% @spec string_to_date(String::string()) -> date()
-string_to_date(String) ->
-    [Year, Month, Day] =
-        case string:tokens(String, "-/") of
-            [Y, M, D] when length(Y) =:= 4 -> [Y, M, D];
-            [M, D, Y] when length(Y) =:= 4 -> [Y, M, D]
-        end,
-    Date = list_to_tuple([list_to_integer(X) || X <- [Year, Month, Day]]),
-    true = calendar:valid_date(Date),
-    Date.
-
-% @spec day_of_week(Date) -> string()
+%% @spec day_of_week(date()) -> string()
+%% @doc Returns the day of the week as a string ("monday".."sunday") for `Date'.
+%% ```
+%% > edate:day_of_week({2010,7,4}).
+%% "sunday"
+%% '''
+-spec day_of_week(date()) -> string().
 day_of_week(Date) ->
     case calendar:day_of_the_week(Date) of
         1 -> "monday";
@@ -119,10 +85,14 @@ day_of_week(Date) ->
         7 -> "sunday"
     end.
 
-% derived from http://www.gmarts.org/index.php?go=415#geteasterdatec
-% converted by Evan Haas <evanhaas@gmail.com>
-% @spec easter(Year::integer()) -> date()
-% @doc returns Date of Easter (Roman Catholic) in the specified year
+%% @spec easter(year()) -> date()
+%% @doc Returns the date for Easter (Roman Catholic) in `Year'. Derived from
+%% [http://www.gmarts.org/index.php?go=415#geteasterdatec].
+%% ```
+%% > edate:easter(2010).
+%% {2010,4,4}
+%% '''
+-spec easter(year()) -> date().
 easter(Year) ->
     NCent = Year div 100,
     NRemain19 = Year rem 19,
@@ -147,89 +117,133 @@ fix(NCent, N1) when NCent == 33 orelse NCent == 36 orelse NCent == 37 -> N1 - 2;
 fix(NCent, N1) when NCent > 26 -> N1 - 1;
 fix(_NCent, N1) -> N1.
 
+%% @spec end_of_month(Date::date()) -> date()
+%% @doc Returns the last date of the month containing `Date'.
+%% ```
+%% > edate:end_of_month({2010,2,15}).
+%% {2010,2,28}
+%% '''
+-spec end_of_month(date()) -> date().
+end_of_month({Y, M, _D}) -> {Y, M, calendar:last_day_of_the_month(Y, M)}.
+
+%% @spec is_in_future(date()) -> bool()
+%% @doc Returns `true' if `Date' is in the future, and `false' otherwise.
+%% ```
+%% > edate:is_in_future({3000,7,1}).
+%% true
+%% '''
+-spec is_in_future(date()) -> boolean().
+is_in_future(Date) -> subtract(Date, today()) > 0.
+
+%% @spec is_in_past(date()) -> bool()
+%% @doc Returns `true' if `Date' is in the past, and `false' otherwise.
+%% ```
+%% > edate:is_in_past({1950,7,1}).
+%% true
+%% '''
+-spec is_in_past(date()) -> boolean().
+is_in_past(Date) -> subtract(Date, today()) < 0.
+
+%% @spec shift(integer(), period()) -> date()
+%% @doc Returns a new date after shifting today's date by `N' periods.
+-spec shift(integer(), period()) -> date().
+shift(N, Period) -> shift(date(), N, Period).
+
+%% @spec shift(date(), integer(), period()) -> date()
+%% @doc Returns a new date after shifting `Date' by `N' periods.
+%% ```
+%% > edate:shift({2000,1,1}, -2, days).
+%% {1999,12,30}
+%% > edate:shift({2010,2,27}, 1, week).
+%% {2010,3,6}
+%% '''
+-spec shift(date(), integer(), period()) -> date().
+shift(Date, N, Period) when Period =:= day; Period =:= days ->
+    calendar:gregorian_days_to_date(calendar:date_to_gregorian_days(Date) + N);
+shift(Date, N, Period) when Period =:= week; Period =:= weeks ->
+    shift(Date, 7*N, days);
+shift(Date, N, Period) when Period =:= year; Period =:= years ->
+    shift(Date, 12*N, months);
+shift({Y, M, D}, N, Period) when Period =:= month; Period =:= months ->
+    %% in order for the modular arithmetic to work, months in this function range
+    %% from 0 to 11 (January to December)
+    TotalMonths = 12*Y + M-1 + N,
+    Month = TotalMonths rem 12,
+    Year = (TotalMonths - Month) div 12,
+    %% add one back to the month to fix our mod 12 shenanigans
+    find_valid_date({Year, Month+1, D}).
+
+%% @spec find_valid_date(date()) -> date()
+%% @doc Returns `Date' if valid. Otherwise, backward searches for a valid date.
+-spec find_valid_date(date()) -> date().
+find_valid_date(Date) ->
+    case calendar:valid_date(Date) of
+        true -> Date;
+        false ->
+            {Y, M, D} = Date,
+            find_valid_date({Y, M, D-1})
+    end.
+
+%% @spec string_to_date(string()) -> date()
+%% @doc Converts `String' to a date. The following string formats are valid
+%% (with or without zero-padding):
+%% YYYY-MM-DD, YYYY/MM/DD, MM-DD-YYYY, MM/DD/YYYY.
+%% ```
+%% > edate:string_to_date("1976-03-18").
+%% {1976,3,18}
+%% > edate:string_to_date("3/18/1976").
+%% {1976,3,18}
+%% '''
+-spec string_to_date(string()) -> date().
+string_to_date(String) ->
+    [Year, Month, Day] =
+        case string:tokens(String, "-/") of
+            [Y, M, D] when length(Y) =:= 4 -> [Y, M, D];
+            [M, D, Y] when length(Y) =:= 4 -> [Y, M, D]
+        end,
+    Date = list_to_tuple([list_to_integer(X) || X <- [Year, Month, Day]]),
+    true = calendar:valid_date(Date),
+    Date.
+
+%% @spec subtract(date(), date()) -> integer()
+%% @doc Returns `Date1' - `Date2' as an integer number of days. The number of
+%% days returned will be positive if `Date1' > `Date2'.
+%% ```
+%% > edate:subtract({2010,7,4}, {2010,7,1}).
+%% 3
+%% > edate:subtract({2010,7,1}, {2010,7,4}).
+%% -3
+%% '''
+-spec subtract(date(), date()) -> integer().
+subtract(Date1, Date2) ->
+    calendar:date_to_gregorian_days(Date1) -
+    calendar:date_to_gregorian_days(Date2).
+
+%% @spec today() -> date()
+%% @doc Returns today's date.
+-spec today() -> date().
+today() -> date().
+
+%% @spec tomorrow() -> date()
+%% @doc Returns tomorrow's date.
+-spec tomorrow() -> date().
+tomorrow() -> shift(1, day).
+
+%% @spec yesterday() -> date()
+%% @doc Returns yesterday's date.
+-spec yesterday() -> date().
+yesterday() -> shift(-1, day).
+
 -ifdef(TEST).
-
-shift_test_() ->
-    [?_assertEqual(date(), shift(0, days)),
-     ?_assertEqual(date(), shift(0, days)),
-     ?_assertEqual(date(), shift(0, months)),
-     ?_assertEqual(date(), shift(0, years)),
-     % relative dates
-     ?_assertEqual(tomorrow(), shift(1, day)),
-     ?_assertEqual(yesterday(), shift(-1, day)),
-     % simple day addition
-     ?_assertEqual({2000,1,2}, shift({2000,1,1}, 1, day)),
-     ?_assertEqual({2000,1,3}, shift({2000,1,1}, 2, days)),
-     % simple week addition
-     ?_assertEqual({2000,1,8}, shift({2000,1,1}, 1, week)),
-     ?_assertEqual({2000,1,15}, shift({2000,1,1}, 2, weeks)),
-     % simple month addition
-     ?_assertEqual({2000,2,1}, shift({2000,1,1}, 1, month)),
-     ?_assertEqual({2000,3,1}, shift({2000,1,1}, 2, months)),
-     % simple year addition
-     ?_assertEqual({2003,1,1}, shift({2000,1,1}, 3, years)),
-     % simple year subtraction
-     ?_assertEqual({1997,1,1}, shift({2000,1,1}, -3, years)),
-     % day subtraction at year boundary
-     ?_assertEqual({1999,12,31}, shift({2000,1,1}, -1, day)),
-     ?_assertEqual({1999,12,30}, shift({2000,1,1}, -2, days)),
-     % week subtraction at year boundary
-     ?_assertEqual({1999,12,25}, shift({2000,1,1}, -1, week)),
-     ?_assertEqual({1999,12,18}, shift({2000,1,1}, -2, weeks)),
-     % month subtraction at year boundary
-     ?_assertEqual({1999,12,1}, shift({2000,1,1}, -1, month)),
-     ?_assertEqual({1999,11,1}, shift({2000,1,1}, -2, months)),
-     % 1 year = 12 months = 365 days (in a non-leap year)
-     ?_assertEqual(shift({2001,5,10}, 1, year), shift({2001,5,10}, 12, months)),
-     ?_assertEqual(shift({2001,5,10}, 1, year), shift({2001,5,10}, 365, days)),
-     % date rounding from month addition and subtraction
-     ?_assertEqual({2001,2,28}, shift({2001,1,31}, 1, month)),
-     ?_assertEqual({2001,2,28}, shift({2001,3,31}, -1, month)),
-     % leap year
-     ?_assertEqual({2012,2,29}, shift({2012,1,31}, 1, month)),
-     ?_assertEqual({2012,2,29}, shift({2012,4,30}, -2, months)),
-     ?_assertEqual({2013,2,28}, shift({2012,2,29}, 1, year))].
-
-subtract_test_() ->
-    [?_assertEqual(3, subtract({2010,7,4}, {2010,7,1})),
-     ?_assertEqual(-3, subtract({2010,7,1}, {2010,7,4})),
-     ?_assertEqual(1, subtract(today(), yesterday())),
-     ?_assertEqual(0, subtract(today(), today()))].
-
-is_in_future_test_() ->
-    [?_assertEqual(true, is_in_future(tomorrow())),
-     ?_assertEqual(false, is_in_future(yesterday())),
-     ?_assertEqual(false, is_in_future(today()))].
-
-is_in_past_test_() ->
-    [?_assertEqual(false, is_in_past(tomorrow())),
-     ?_assertEqual(true, is_in_past(yesterday())),
-     ?_assertEqual(false, is_in_past(today()))].
 
 beginning_of_month_test_() ->
     [?_assertEqual({2012,2,1}, beginning_of_month({2012,2,15})),
      ?_assertEqual({2012,2,1}, beginning_of_month({2012,2,1}))].
 
-end_of_month_test_() ->
-    [?_assertEqual({2012,2,29}, end_of_month({2012,2,29})),
-     ?_assertEqual({2012,2,29}, end_of_month({2012,2,1})),
-     ?_assertEqual({2010,2,28}, end_of_month({2010,2,28})),
-     ?_assertEqual({2010,2,28}, end_of_month({2010,2,1}))].
-
 date_to_string_test_() ->
     [?_assertEqual("1976-12-20", date_to_string({1976,12,20})),
      ?_assertEqual("1976-03-18", date_to_string({1976,3,18})),
      ?_assertEqual("2010-01-02", date_to_string({2010,1,2}))].
-
-string_to_date_test_() ->
-    [?_assertEqual({2010,1,2}, string_to_date("2010-01-02")),
-     ?_assertEqual({2010,1,2}, string_to_date("2010-1-2")),
-     ?_assertEqual({2010,1,2}, string_to_date("2010/01/02")),
-     ?_assertEqual({2010,1,2}, string_to_date("2010/1/2")),
-     ?_assertEqual({2010,1,2}, string_to_date("01-02-2010")),
-     ?_assertEqual({2010,1,2}, string_to_date("1-2-2010")),
-     ?_assertEqual({2010,1,2}, string_to_date("01/02/2010")),
-     ?_assertEqual({2010,1,2}, string_to_date("1/2/2010"))].
 
 day_of_week_test_() ->
     [?_assertEqual("monday", day_of_week({2010,6,28})),
@@ -248,5 +262,78 @@ easter_test_() ->
      ?_assertEqual({2012,4,8}, easter(2012)),
      ?_assertEqual({2013,3,31}, easter(2013)),
      ?_assertEqual({2014,4,20}, easter(2014))].
+
+end_of_month_test_() ->
+    [?_assertEqual({2012,2,29}, end_of_month({2012,2,29})),
+     ?_assertEqual({2012,2,29}, end_of_month({2012,2,1})),
+     ?_assertEqual({2010,2,28}, end_of_month({2010,2,28})),
+     ?_assertEqual({2010,2,28}, end_of_month({2010,2,1}))].
+
+is_in_future_test_() ->
+    [?_assertEqual(true, is_in_future(tomorrow())),
+     ?_assertEqual(false, is_in_future(yesterday())),
+     ?_assertEqual(false, is_in_future(today()))].
+
+is_in_past_test_() ->
+    [?_assertEqual(false, is_in_past(tomorrow())),
+     ?_assertEqual(true, is_in_past(yesterday())),
+     ?_assertEqual(false, is_in_past(today()))].
+
+shift_test_() ->
+    [?_assertEqual(date(), shift(0, days)),
+     ?_assertEqual(date(), shift(0, days)),
+     ?_assertEqual(date(), shift(0, months)),
+     ?_assertEqual(date(), shift(0, years)),
+     %% relative dates
+     ?_assertEqual(tomorrow(), shift(1, day)),
+     ?_assertEqual(yesterday(), shift(-1, day)),
+     %% simple day addition
+     ?_assertEqual({2000,1,2}, shift({2000,1,1}, 1, day)),
+     ?_assertEqual({2000,1,3}, shift({2000,1,1}, 2, days)),
+     %% simple week addition
+     ?_assertEqual({2000,1,8}, shift({2000,1,1}, 1, week)),
+     ?_assertEqual({2000,1,15}, shift({2000,1,1}, 2, weeks)),
+     %% simple month addition
+     ?_assertEqual({2000,2,1}, shift({2000,1,1}, 1, month)),
+     ?_assertEqual({2000,3,1}, shift({2000,1,1}, 2, months)),
+     %% simple year addition
+     ?_assertEqual({2003,1,1}, shift({2000,1,1}, 3, years)),
+     %% simple year subtraction
+     ?_assertEqual({1997,1,1}, shift({2000,1,1}, -3, years)),
+     %% day subtraction at year boundary
+     ?_assertEqual({1999,12,31}, shift({2000,1,1}, -1, day)),
+     ?_assertEqual({1999,12,30}, shift({2000,1,1}, -2, days)),
+     %% week subtraction at year boundary
+     ?_assertEqual({1999,12,25}, shift({2000,1,1}, -1, week)),
+     ?_assertEqual({1999,12,18}, shift({2000,1,1}, -2, weeks)),
+     %% month subtraction at year boundary
+     ?_assertEqual({1999,12,1}, shift({2000,1,1}, -1, month)),
+     ?_assertEqual({1999,11,1}, shift({2000,1,1}, -2, months)),
+     %% 1 year = 12 months = 365 days (in a non-leap year)
+     ?_assertEqual(shift({2001,5,10}, 1, year), shift({2001,5,10}, 12, months)),
+     ?_assertEqual(shift({2001,5,10}, 1, year), shift({2001,5,10}, 365, days)),
+     %% date rounding from month addition and subtraction
+     ?_assertEqual({2001,2,28}, shift({2001,1,31}, 1, month)),
+     ?_assertEqual({2001,2,28}, shift({2001,3,31}, -1, month)),
+     %% leap year
+     ?_assertEqual({2012,2,29}, shift({2012,1,31}, 1, month)),
+     ?_assertEqual({2012,2,29}, shift({2012,4,30}, -2, months)),
+     ?_assertEqual({2013,2,28}, shift({2012,2,29}, 1, year))].
+
+string_to_date_test_() ->
+    [?_assertEqual({2010,1,2}, string_to_date("2010-01-02")),
+     ?_assertEqual({2010,1,2}, string_to_date("2010-1-2")),
+     ?_assertEqual({2010,1,2}, string_to_date("2010/01/02")),
+     ?_assertEqual({2010,1,2}, string_to_date("2010/1/2")),
+     ?_assertEqual({2010,1,2}, string_to_date("01-02-2010")),
+     ?_assertEqual({2010,1,2}, string_to_date("1-2-2010")),
+     ?_assertEqual({2010,1,2}, string_to_date("01/02/2010")),
+     ?_assertEqual({2010,1,2}, string_to_date("1/2/2010"))].
+
+subtract_test_() ->
+    [?_assertEqual(3, subtract({2010,7,4}, {2010,7,1})),
+     ?_assertEqual(-3, subtract({2010,7,1}, {2010,7,4})),
+     ?_assertEqual(1, subtract(today(), yesterday())),
+     ?_assertEqual(0, subtract(today(), today()))].
 
 -endif.
